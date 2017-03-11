eb791fabcc16b55d52568c092a5ee2f54520dc65
AdamDeJames <adamdejames@gmail.com>
2017-03-11 20:04:13 +0900

    HUD revamped

--- worldrp/gamemode/cl_hud.lua
+++ worldrp/gamemode/cl_hud.lua
@@ -13,28 +13,75 @@ end)
 
 
 function wrp_hud.DrawHud()
-	wrp_hud.HP = LocalPlayer():Health()
-	wrp_hud.Arm = LocalPlayer():Armor()
-	wrp_hud.Wep1 = LocalPlayer():GetActiveWeapon()
+    wrp_hud.HP = LocalPlayer():Health()
+    wrp_hud.Arm = LocalPlayer():Armor()
+    if(IsValid(LocalPlayer():GetActiveWeapon())) then
+        wrp_hud.Wep1 = LocalPlayer():GetActiveWeapon()
+    end
 
 
-	// Base Frame
-	draw.RoundedBox(4, 0, ScrH()-185, 300, 300, Color(10, 10, 10, 200))
-	//draw.RoundedBox(4, 95, ScrH()-180, 100, 20, Color(10, 10, 10, 230))
-	draw.SimpleText("WorldRP", "ChatFont", 110, ScrH()-180, Color(255, 255, 255, 255))
+    // Base Frame
+    draw.RoundedBox(4, 0, ScrH()-150, 330, 150, Color(10, 10, 10, 200))
+    //draw.RoundedBox(4, 95, ScrH()-180, 100, 20, Color(10, 10, 10, 230))
+    //draw.SimpleText(string.sub(GetHostName(), 1, 25), "ChatFont", ScrW()-1800, ScrH()-180, Color(255, 255, 255, 255))
 
-	// Print RP name -- note does not work yet, will default to steam name
-	surface.SetDrawColor(255, 255, 255, 255)
-	surface.SetMaterial(Material("icon16/user.png"))
-	surface.DrawTexturedRect(10, ScrH()-150, 16, 16)
-	if(not LocalPlayer():GetNWString("worldrp_rpname") or LocalPlayer():GetNWString("worldrp_rpname") == "") then
-		draw.SimpleText("Name: "..LocalPlayer():Name(), "ChatFont", 30, ScrH()-150, Color(255, 255, 255, 255))
-	end
-	// Money
-	surface.SetDrawColor(255,255,255,255)
-	surface.SetMaterial(Material("icon16/money.png"))
-	surface.DrawTexturedRect(10, ScrH()-130, 16, 16)
-	draw.SimpleText("Wallet: $", "ChatFont", 30, ScrH()-130, Color(255, 255, 255, 255))
-	draw.SimpleText(tostring(LocalPlayer():GetNWInt("worldrp_cash")), "ChatFont", 100, ScrH()-130, Color(0, 255, 0, 255))
+    // Print RP name
+    surface.SetDrawColor(255, 255, 255, 255)
+    surface.SetMaterial(Material("icon16/user.png"))
+    surface.DrawTexturedRect(ScrW()-1910, ScrH()-130, 16, 16)
+    draw.SimpleText("Name: "..string.sub(LocalPlayer():Nick(), 1, 20), "ChatFont", ScrW()-1890, ScrH()-130, Color(255, 255, 255, 255))
+
+    // Money
+    surface.SetDrawColor(255,255,255,255)
+    surface.SetMaterial(Material("icon16/money.png"))
+    surface.DrawTexturedRect(ScrW()-1910, ScrH()-110, 16, 16)
+    draw.SimpleText("Wallet:", "ChatFont", ScrW()-1890, ScrH()-110, Color(255, 255, 255, 255))
+    draw.SimpleText(LocalPlayer():GetNWInt("worldrp_cash"), "ChatFont", ScrW()-1830, ScrH()-110, Color(0, 255, 0, 255))
+
+    //Salary
+    surface.SetDrawColor(255,255,255,255)
+    surface.SetMaterial(Material("icon16/money_add.png"))
+    surface.DrawTexturedRect(ScrW()-1910, ScrH()-90, 16, 16)
+    draw.SimpleText("Error", "ChatFont", ScrW()-1890, ScrH()-90, Color(255, 255, 255, 255))
+
+    //Job
+    surface.SetDrawColor(255,255,255,255)
+    surface.SetMaterial(Material("icon16/wrench.png"))
+    surface.DrawTexturedRect(ScrW()-1910, ScrH()-70, 16, 16)
+    draw.SimpleText("Occupation: "..team.GetName(LocalPlayer():Team()), "Chatfont", ScrW()-1890, ScrH()-70)
+    // Health
+    draw.RoundedBox(4, ScrW()-1920, ScrH()-180, 110, 30, Color(10, 10, 10, 200))
+    draw.RoundedBox(4, ScrW()-1915, ScrH()-175, wrp_hud.HP, 20, Color(187, 37, 0, 255))
+    draw.SimpleText("Health: "..wrp_hud.HP,"ChatFont", ScrW()-1910, ScrH()-175, Color(255, 255, 255, 255))
+
+    // Armor
+    if(wrp_hud.Arm > 0) then
+        draw.RoundedBox(4, ScrW()-1810, ScrH()-180, 110, 30, Color(10, 10, 10, 200))
+        draw.RoundedBox(4, ScrW()-1805, ScrH()-175, math.Clamp(wrp_hud.Arm, 0, 100), 20, Color(0, 0, 150, 255))
+        draw.SimpleText("Armor: "..wrp_hud.Arm,"ChatFont", ScrW()-1800, ScrH()-175, Color(255, 255, 255, 255))
+    end
+
+    // Ammo
+    if(wrp_hud.Arm > 0) then
+        if(LocalPlayer():Alive() != false and IsValid(wrp_hud.Wep1)) then
+            if(wrp_hud.Wep1:GetPrimaryAmmoType() >= 0) then
+                draw.RoundedBox(4, ScrW()-1700, ScrH()-180, 110, 30, Color(10, 10, 10, 200))
+                draw.RoundedBox(4, ScrW()-1695, ScrH()-175, math.Clamp(wrp_hud.Wep1:Clip1() * 6, 2, 100), 20, Color(0, 0, 150, 255))
+                if(wrp_hud.Wep1:Clip1() > 0) then
+                    draw.SimpleText("Ammo: "..wrp_hud.Wep1:Clip1().."/"..LocalPlayer():GetAmmoCount(wrp_hud.Wep1:GetPrimaryAmmoType()),"ChatFont", ScrW()-1690, ScrH()-175, Color(255, 255, 255, 255))
+                else
+                    draw.SimpleText("No Ammo", "ChatFont", ScrW()-1690, ScrH()-175, Color(255, 255, 255, 255))
+                end
+            end
+        end
+    else
+        draw.RoundedBox(4, ScrW()-1810, ScrH()-180, 110, 30, Color(10, 10, 10, 200))
+        draw.RoundedBox(4, ScrW()-1805, ScrH()-175, math.Clamp(wrp_hud.Wep1:Clip1() * 6, 2, 100), 20, Color(0, 0, 150, 255))
+        if(wrp_hud.Wep1:Clip1() > 0) then
+             draw.SimpleText("Ammo: "..wrp_hud.Wep1:Clip1().."/"..LocalPlayer():GetAmmoCount(wrp_hud.Wep1:GetPrimaryAmmoType()),"ChatFont", ScrW()-1800, ScrH()-175, Color(255, 255, 255, 255))
+        else
+            draw.SimpleText("No Ammo", "ChatFont", ScrW()-1800, ScrH()-175, Color(255, 255, 255, 255))
+        end
+    end
 end
 hook.Add("HUDPaint", "paintthehudufuck", wrp_hud.DrawHud)
